57c97356153902f3396670b1c4d868a9beddb3b1
Brown, Ashton <abro5723@gmail.com>
2016-01-19 09:41:41 -0700

    C++ creation
    Small project dealing with I/O

---
+++ src/Controller/Runner.cpp
@@ -0,0 +1,17 @@
+
+/*
+ * Runner.cpp
+ *
+ *  Created on: Jan 19, 2016
+ *      Author: abro5723
+ */
+
+#include "SillyAppController.h"
+
+int main()
+{
+	SillyAppController * baseController = new SillyAppController();
+	baseController->start();
+	return 0;
+}
+
